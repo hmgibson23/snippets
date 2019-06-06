f25018349d478d646b1f2a82bc2eb482b1652e64
Hugo Gibson <hugo.gibson@yahoo.co.uk>
2019-06-06 16:59:07 +0100

    fun

--- emacs/.emacs.d/tmp/build-irony-server-1.3.1/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hugo/.emacs.d/.cask/26.2/elpa/irony-20190125.1234/server/test
-# Build directory: /home/hugo/.emacs.d/tmp/build-irony-server-1.3.1/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("elisp")
